b5baabea3acbf0b9b446e5c6147c4addbfc663ac
다운 <j_d_woon@naver.com>
2024-05-06 15:11:45 +0900

    Initial Commit

---
+++ src/api/v1/v1.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { RoomController } from './room/room.controller';
+import { RoomModule } from './room/room.module';
+import { FavoriteService } from './favorite/favorite.service';
+import { FavoriteController } from './favorite/favorite.controller';
+import { FavoriteModule } from './favorite/favorite.module';
+
+@Module({
+  controllers: [UserController, RoomController, FavoriteController],
+  imports: [UserModule, RoomModule, FavoriteModule],
+  providers: [FavoriteService],
+})
+export class V1Module {}
