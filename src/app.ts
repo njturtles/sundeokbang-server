7b72e8c4a9405d2254173ecfdc58503fe0c4008d
다운 <j_d_woon@naver.com>
2024-07-04 16:19:00 +0900

    feat: ✨ cors 설정
    - 로컬환경에서 cors 정책 위반으로 인한 오류 발생 해결

--- src/app.ts
+++ src/app.ts
@@ -11,9 +11,24 @@ async function bootstrap() {
     const env = app.get(ConfigService);
     const port = env.get('APP_PORT');
 
+    const allowedOrigins = [
+        'http://localhost:3000',
+        'https://api.dev.borangkkae.com',
+    ];
+
     app.use(cookieParser());
 
-    app.enableCors({ credentials: true });
+    app.enableCors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        credentials: true,
+    });
+
     app.use(httpContext.middleware);
 
     app.enableVersioning({
