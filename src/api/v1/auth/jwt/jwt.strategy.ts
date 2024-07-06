867046f0c1d1b6a3f7ecefd94f22a6cd0d0d6813
다운 <j_d_woon@naver.com>
2024-07-06 15:52:34 +0900

    refactor: 🍻 인가 방법 변경
    - req.cookies를 사용하지 않고 req.headers.authorization를 사용하여 인가 방법 변경

--- src/api/v1/auth/jwt/jwt.strategy.ts
+++ src/api/v1/auth/jwt/jwt.strategy.ts
@@ -3,7 +3,6 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserService } from '../../user/user.service';
-import { Request } from 'express';
 import ApiError from '../../../../libs/common-config/res/api.error';
 import ApiCodes from '../../../../libs/common-config/res/api.codes';
 import ApiMessages from '../../../../libs/common-config/res/api.messages';
@@ -15,22 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         private readonly userService: UserService,
     ) {
         super({
-            jwtFromRequest: ExtractJwt.fromExtractors([
-                (request: Request) => {
-                    const token = request?.cookies?.user;
-                    if (!token) {
-                        throw new ApiError(
-                            ApiCodes.UNAUTHORIZED,
-                            ApiMessages.UNAUTHORIZED,
-                            {
-                                message:
-                                    'No authentication token found in cookies',
-                            },
-                        );
-                    }
-                    return token;
-                },
-            ]),
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
             secretOrKey: env.get<string>('JWT_SECRET'),
         });
